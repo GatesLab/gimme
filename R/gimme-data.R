96350e1e3f3f5044118b15384ff86a71a59a51c5
Teague Henry <trhenry@live.unc.edu>
2015-09-18 09:59:35 -0400

    Initial Commit for Development Branch

---
+++ R/gimme-data.R
@@ -0,0 +1,55 @@
+#' ts1
+#'
+#' This data contains simulated time-series data for a single individual with 50 time points and 3 variables, or regions of interest.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ts1
+#' @usage ts1
+#' @format A data frame with 50 observations on 3 variables.
+NULL
+
+#' ts2
+#'
+#' This data contains simulated time-series data for a single individual with 50 time points and 3 variables, or regions of interest.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ts2
+#' @usage ts2
+#' @format A data frame with 50 observations on 3 variables.
+NULL
+
+#' ts3
+#'
+#' This data contains simulated time-series data for a single individual with 50 time points and 3 variables, or regions of interest.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ts3
+#' @usage ts3
+#' @format A data frame with 50 observations on 3 variables.
+NULL
+
+#' ts4
+#'
+#' This data contains simulated time-series data for a single individual with 50 time points and 3 variables, or regions of interest.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ts4
+#' @usage ts4
+#' @format A data frame with 50 observations on 3 variables.
+NULL
+
+#' ts5
+#'
+#' This data contains simulated time-series data for a single individual with 50 time points and 3 variables, or regions of interest.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ts5
+#' @usage ts5
+#' @format A data frame with 50 observations on 3 variables.
+NULL
+
---
+++ R/gimme-pkg.R
@@ -0,0 +1,27 @@
+#' @name gimme-package
+#' @aliases gimme-package
+#' @title Group iterative multiple model estimation
+#' @description This package contains functions to identify group- and individual-level structural equation models.
+#' @author Stephanie Lane [aut, cre, trl], \cr
+#' Kathleen Gates [aut], \cr
+#' Peter Molenaar [aut] \cr
+#' Maintainer: Stephanie Lane \email{slane@@unc.edu}
+#' @import qgraph lavaan igraph
+#' @importFrom grDevices dev.off pdf
+#' @importFrom stats aggregate ave complete.cases qchisq qnorm reshape time
+#' @importFrom utils capture.output head read.table write.csv write.table
+#' @keywords gimme
+#' @details Researchers across varied domains gather multivariate data for each individual unit of study
+#' across multiple occasions of measurement. Generally referred to as time series
+#' (or in the social sciences, intensive longitudinal) data, examples include
+#' psychophysiological processes such as neuroimaging and heart rate variability,
+#' daily diary studies, and observational coding of social interactions among dyads.
+#' A primary goal for acquiring these data is to understand temporal processes.
+#' The gimme package contains several functions for use with these data.
+#' These functions include \code{\link{gimme}}, which provides both group-
+#'  and individual-level results by looking across individuals for patterns of
+#'   relations among variables. A function that provides group-level results,
+#'   \code{\link{aggSEM}}, is included, as well as a function that provides
+#'    individual-level results, \code{\link{indSEM}}. The major functions within the gimme package all require the
+#'    user to specify the directory containing the data and a directory for output to be stored.
+NULL
