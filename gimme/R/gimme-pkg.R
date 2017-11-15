5feac85fd0d8e83906c1471085ace589d0b00f4f
stlane <slane@unc.edu>
2017-11-14 20:09:48 -0500

    Updating namespace
    Added igraph membership() function to namespace.

--- gimme/R/gimme-pkg.R
+++ gimme/R/gimme-pkg.R
@@ -16,7 +16,7 @@
 #' @importFrom stats aggregate ave complete.cases qchisq qnorm reshape time cov sd na.omit
 #' @importFrom utils capture.output head read.table write.csv write.table file_test read.csv
 #' @importFrom qgraph qgraph
-#' @importFrom igraph graph.adjacency walktrap.community modularity
+#' @importFrom igraph graph.adjacency walktrap.community modularity membership
 #' @importFrom graphics plot
 #' @keywords gimme
 #' @details Researchers across varied domains gather multivariate data for each individual unit of study
