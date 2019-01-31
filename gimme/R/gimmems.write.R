08f996a350e13707927b77e763625c0ce53da1d2
Zachary Fisher <zackfisher@users.noreply.github.com>
2019-01-31 18:11:40 -0500

    Update MS-GIMME output.

--- gimme/R/gimmems.write.R
+++ gimme/R/gimmems.write.R
@@ -4,7 +4,60 @@ gimmems.write <- function(x){
   
   out.dir <- x$dat$ctrlOpts$out
   
-  df <- as.data.frame(do.call("rbind", 
+  #------------------------------------------#
+  # Summary fit
+  #------------------------------------------#
+  
+  summaryFit <- as.data.frame(do.call("rbind", 
+    
+    # grpsol_i                       
+    lapply(seq_along(x$ind_fit), function(i){                
+      
+      as.data.frame(do.call("rbind",
+                            
+        # grpsol_i_ind_j
+        lapply(seq_along(x$ind_fit[[i]]), function(j){         
+          
+          as.data.frame(do.call("rbind",
+                                        
+            # grpsol_i_ind_j_indsol_k
+            lapply(seq_along(x$ind_fit[[i]][[j]]), function(k){  
+            
+              grp_sol_i_ind_j_sol_k <-  x$ind_fit[[i]][[j]][[k]]
+              
+              # we want to add some identifying information
+              id_cols <- data.frame(
+                "subj" = grp_sol_i_ind_j_sol_k$subj,
+                "grp_sol" = i,
+                "ind_sol" = k
+              )
+              
+              df <- cbind(id_cols,  t(data.frame(grp_sol_i_ind_j_sol_k$fits)))
+              
+              # add model fit information
+              #df <- cbind(df, t(data.frame(grp_sol_i_ind_j_sol_k$fits)))
+              
+              df$status <- grp_sol_i_ind_j_sol_k$status
+              
+              df
+            
+            })
+            
+          ))
+          
+        })
+      
+      ))
+      
+    })
+    
+  ))
+  
+  # now let's write this df to the ms directory
+  write.csv(summaryFit, file.path(out.dir, "summaryFit.csv"), row.names=FALSE)
+  
+  
+  indivPathEstimates <- as.data.frame(do.call("rbind", 
     
     # grpsol_i                       
     lapply(seq_along(x$ind_fit), function(i){                
@@ -39,7 +92,14 @@ gimmems.write <- function(x){
               df <- cbind(id_cols, df)
               
               # add model fit information
-              df <- cbind(df, t(data.frame(grp_sol_i_ind_j_sol_k$fits)))
+              #df <- cbind(df, t(data.frame(grp_sol_i_ind_j_sol_k$fits)))
+              
+              df$op <- df$ci.lower <- df$ci.upper <- NULL
+              
+              
+              colnames(df) <-c("subj", "grp_sol", "ind_sol", 
+                               "dv", "iv", "beta", "se", 
+                               "z", "pval")
               
               df
             
@@ -56,8 +116,8 @@ gimmems.write <- function(x){
   ))
   
   # now let's write this df to the ms directory
-  write.csv(df, file.path(out.dir, "MS-GIMME_Results.csv"), row.names=FALSE)
+  write.csv(indivPathEstimates, file.path(out.dir, "indivPathEstimates.csv"), row.names=FALSE)
   
-  invisible(df)
+  invisible(indivPathEstimates)
 
 }
