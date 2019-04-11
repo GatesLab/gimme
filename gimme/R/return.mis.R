844554e7afa82eb993a0ee3708d6f75c44eb7009
kgates <gateskm@email.unc.edu>
2019-04-11 17:12:29 -0400

    return.mis fix
    obtain all possible MIs from elig_paths. Fixes issue with referring to "dat$candidate_cor" , which wasn't read into the leader functions

--- gimme/R/return.mis.R
+++ gimme/R/return.mis.R
@@ -3,7 +3,7 @@
 #' @return If successful, returns MIs for an individual. If unsuccessful, 
 #' returns NA.
 #' @keywords internal 
-return.mis <- function(fit){
+return.mis <- function(fit, elig_paths){
   zero_se  <- FALSE
   no_paths <- FALSE
   error    <- any(grepl("error", class(fit)))
@@ -20,15 +20,15 @@ return.mis <- function(fit){
     #                             standardized = FALSE,
     #                             sort. = FALSE), 
     #                  error = function(e) e)
-    
-    lanMod <- function(fit){
+    # 
+    lanMod <- function(fit, elig_paths){
       mis0 <- modindices(fit, standardized = FALSE, sort. = FALSE)
       mis0_idx <- paste0(mis0$lhs,mis0$op,mis0$rhs)
-      mis <- mis0[mis0$op == "~" | mis0_idx %in% dat$candidate_corr,]
+      mis <- mis0[mis0_idx %in% elig_paths,]
       return(mis)
     }
     
-    mis   <- tryCatch(lanMod(fit),
+    mis   <- tryCatch(lanMod(fit, elig_paths),
                       error = function(e) e)
     error <- any(grepl("error", class(mis)))
     if (error) mis <- NA 
