26bde38a3c5a78ec7883d318f7522f10bb71fc68
Lan Luo <lanl6277@gmail.com>
2020-02-12 10:28:29 -0500

    sub_plots_cov storing duplicates fixed

--- gimme/R/final.org.R
+++ gimme/R/final.org.R
@@ -185,12 +185,14 @@ final.org <- function(dat, grp, sub, sub_spec, diagnos=FALSE, store){
                                               "PathCountsMatrix.csv")), 
                       row.names = TRUE)
           }
+          if (dat$plot & sub_spec[[s]]$n_sub_subj != 1){ ##add by lan 021220: store the sub_plot & sub_plot_cov to the plots when n>1
           sub_coefs[[s]] <- sub_s_coefs
           sub_summ[[s]]  <- sub_s_summ
           sub_plots[[s]] <- sub_plot
           sub_plots_cov[[s]] <- sub_plot_cov
           sub_counts[[s]] <- sub_s_mat_counts
           sub_counts_cov[[s]] <- sub_s_mat_counts_cov
+          }
         }
         
         summ <- do.call("rbind", sub_summ)
