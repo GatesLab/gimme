2834ed6bf911bdeea0f2eb9a6761375b22398590
JPark93 <jjp409@psu.edu>
2020-10-12 13:51:37 -0700

    Switched search argument in SGIMME to optimize conductance rather than modularity

--- gimme/R/sparsity.optimization.R
+++ gimme/R/sparsity.optimization.R
@@ -1,27 +1,61 @@
 #' @keywords internal 
 modmax <- function(x, m, sub_method){
       # sparsify m based on x
-      m[which(m <= stats::quantile(m[upper.tri(m, diag = FALSE)], x))] <- 0
       diag(m) <- 0
+      m[which(m <= stats::quantile(m[upper.tri(m, diag = FALSE)], x))] <- 0
+      olm = m
       m = graph_from_adjacency_matrix(m)
       weights      <- E(m)$weight
         if (sub_method == "Walktrap")
-          modval = modularity(cluster_walktrap(m, weights = weights, steps = 4))
+          p = cluster_walktrap(m, weights = weights, steps = 4)
         if (sub_method == "Infomap")
-          modval = modularity(cluster_infomap(m, e.weights = weights))
+          p = cluster_infomap(m, e.weights = weights)
         if (sub_method == "Edge Betweenness")
-          modval = modularity(cluster_edge_betweenness(m, weights = weights))
+          p = cluster_edge_betweenness(m, weights = weights)
         if (sub_method == "Fast Greedy")
-          modval = modularity(cluster_fast_greedy(m, weights = weights))
+          p = cluster_fast_greedy(m, weights = weights)
         if (sub_method == "Label Prop")
-          modval = modularity(cluster_label_prop(m, weights = weights))
+          p = cluster_label_prop(m, weights = weights)
         if (sub_method == "Leading Eigen")
-          modval = modularity(cluster_leading_eigen(m, weights = weights))
+          p = cluster_leading_eigen(m, weights = weights)
         if (sub_method == "Louvain")
-          modval = modularity(cluster_louvain(m, weights = weights))
+          p = cluster_louvain(m, weights = weights)
         if (sub_method == "Spinglass")
-          modval = modularity(cluster_spinglass(m, weights = weights))
-      return(-1*modval) # solvers generally want to minimze the 
+          p = cluster_spinglass(m, weights = weights)
+      modval <- igraph::modularity(p)
+      sk = ask = list()
+      mem = p$membership
+      dim = length(mem)
+
+      for(i in 1:dim){
+        sk[i] = 0
+        ask[i] = 0
+        for(j in 1:dim){
+          if(olm[i,j] == 0){NULL}else{
+            if(mem[i] == mem[j]){ask[[i]] = ask[[i]] + olm[i,j]}else{
+              sk[[i]] = sk[[i]] + olm[i,j]
+            }
+          }
+        }
+        sk[[i]] = sk[[i]]/2
+        ask[[i]] = sk[[i]] + ask[[i]]/2
+      }
+      sk = unlist(sk); ask = unlist(ask)
+
+      temp.1 = cbind(mem, sk, ask)
+      
+      clist = matrix(NA, 1, max(mem))
+      for(i in 1:max(temp.1[,1])){
+        if(temp.1[mem == i,2] == 0 && temp.1[mem == i,3] == 0){
+          clist[1,i] = 1
+        }else{
+          clist[1,i] = sum(temp.1[mem == i,2]) / 
+            sum(temp.1[mem == i,3])  
+        }
+      }
+      simplified = round(clist, 3)
+      (conductance = 1 - sum(simplified)/length(simplified))
+      return(-1*conductance)
 }
 
 
