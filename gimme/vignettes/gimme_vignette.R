e9cd956b50d5e05145d658da7ed2054960c67d28
Kathleen Katie Gates <gateskm@email.unc.edu>
2023-06-14 11:42:25 -0400

    Add HRF vignette, update main vignette

--- gimme/vignettes/gimme_vignette.R
+++ gimme/vignettes/gimme_vignette.R
@@ -1,14 +1,15 @@
 ## ---- eval = FALSE-------------------------------------------------------
 #  gimme(                  # can use "gimme" or "gimmeSEM"
-#      data = '',          # source directory where your data are
-#      out = '',           # output directory where you'd like your output to go
-#      sep = "",           # how data are separated. "" for space; "," for comma, "/t" for tab-delimited
-#      header = ,          # TRUE or FALSE, is there a header
-#      ar = TRUE,          # TRUE (default) or FALSE, start with autoregressive paths open
-#      plot = TRUE,        # TRUE (default) or FALSE, generate plots
-#      subgroup = FALSE,   # TRUE or FALSE (default), cluster individuals based on similarities in effects
-#      paths = NULL,       # option to list paths that will be group-level (semi-confirmatory)
-#      groupcutoff = .75,  # the proportion that is considered the majority at the group level
-#      subcutoff = .5      # the proportion that is considered the majority at the subgroup level
-#      )
-
+#        data = '',          # list object or source directory where your data are 
+#        out = '',           # output directory where you'd like your output to go
+#        sep = "",           # how data are separated. "" for space; "," for comma, "/t" for tab-delimited
+#        header = ,          # TRUE or FALSE, is there a header
+#        ar = TRUE,          # TRUE (default) or FALSE, start with autoregressive paths open
+#        plot = TRUE,        # TRUE (default) or FALSE, generate plots
+#        subgroup = FALSE,   # TRUE or FALSE (default), cluster individuals based on similarities in effects
+#        paths = NULL,       # option to list paths that will be group-level (semi-confirmatory)
+#        groupcutoff = .75,  # the proportion that is considered the majority at the group level
+#        subcutoff = .75,     # the proportion that is considered the majority at the subgroup level
+#        VAR       = FALSE,  # TRUE or FALSE (default), option to use VAR model instead of uSEM
+#        hybrid    = FALSE   # TRUE or FALSE (default), option to use hybrid-VAR model instead of uSEM
+#)  
---
+++ gimme/vignettes/hrfgimme_vignette.R
@@ -0,0 +1,12 @@
+## ---- eval = FALSE-------------------------------------------------------
+#  gimme(data = HRFsim,   
+#        ar = TRUE,
+#        standardize = TRUE,
+#        exogenous      = "V5",
+#        conv_vars = "V5", 
+#        conv_length = 16,
+#        conv_interval = 1,
+#        mult_vars      = "V3*V5",
+#        mean_center_mult = TRUE, 
+#        groupcutoff = .75)
+
