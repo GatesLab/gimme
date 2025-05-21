937f13fd9a75d8eb505f7c3658229cb0079814c9
Kathleen Katie Gates <gateskm@email.unc.edu>
2025-05-21 10:19:05 -0400

    Fix fit index check

--- R/count.excellent.R
+++ R/count.excellent.R
@@ -3,10 +3,10 @@
 #' @return The number of fit indices that are excellent.
 #' @keywords internal 
 count.excellent <- function(indices){
-  rmseaE    <- ifelse(indices[4] < .05, 1, 0)
-  srmrE     <- ifelse(indices[5] < .05, 1, 0)
-  nnfiE     <- ifelse(indices[6] > .95, 1, 0)
-  cfiE      <- ifelse(indices[7] > .95, 1, 0)
+  rmseaE    <- ifelse(indices[4] <= .05, 1, 0)
+  srmrE     <- ifelse(indices[5] <= .05, 1, 0)
+  nnfiE     <- ifelse(indices[6] >= .95, 1, 0)
+  cfiE      <- ifelse(indices[7] >= .95, 1, 0)
   excellent <- sum(rmseaE, srmrE, nnfiE, cfiE, na.rm = TRUE)
   return(excellent)
 }
